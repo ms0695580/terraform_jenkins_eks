03d1768dcfcd413e5f43c0f8097d8485769aa0f8
ms0695580 <138789107+ms0695580@users.noreply.github.com>
2024-07-23 10:41:08 +0530

    Update backend.tf

--- Jenkins Server/backend.tf
+++ Jenkins Server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-assgnment-project"
+    bucket = "myfirst-assgnment-project"
     key    = "jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
